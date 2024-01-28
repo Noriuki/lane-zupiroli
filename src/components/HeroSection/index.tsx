effd1c820d234ec4a38d5cd60534bcb7ba630482
Noriuki <lane.zupiroli@devio.com.br>
2024-01-28 18:10:37 -0300

    fix: lotties animation

--- src/components/HeroSection/index.tsx
+++ src/components/HeroSection/index.tsx
@@ -5,7 +5,10 @@ import { Player } from '@lottiefiles/react-lottie-player';
 export default function HeroSection() {
   return (
     <section className="section-full">
-      <Player autoplay loop src="/lotties/dev.json" style={{ width: '45%', minWidth: '250px' }} />
+      <div style={{ width: '350px', height: '350px' }}>
+        <Player autoplay loop src="/lotties/dev.json" style={{ width: '100%', minWidth: '100%' }} />
+      </div>
+
       <div className="space-y-2 sm:my-8 sm:space-y-4">
         <h1 className="text-4xl md:text-8xl text-purple-900 text-center">Lane Zupiroli</h1>
         <h2 className="text-2xl md:text-4xl text-purple-900 text-center">Full Stack Developer</h2>
