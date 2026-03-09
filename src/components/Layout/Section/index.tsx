499f88f0dd564e4afd7642a1e76046d9d0e98fd1
Noriuki <lane.zupiroli@devio.com.br>
2026-03-09 20:04:30 -0300

    fix: smooth scroll

--- src/components/Layout/Section/index.tsx
+++ src/components/Layout/Section/index.tsx
@@ -4,7 +4,10 @@ export default function Section(props: ISectionProps) {
   const { children, ...rest } = props;
 
   return (
-    <section className="w-full flex flex-col min-h-screen items-center justify-center py-20 md:py-28 px-4" {...rest}>
+    <section
+      className="w-full flex flex-col min-h-screen items-center justify-center py-20 md:py-28 px-4 snap-start snap-always"
+      {...rest}
+    >
       <div className="w-full flex flex-col items-center gap-8 md:gap-10">{children}</div>
     </section>
   );
